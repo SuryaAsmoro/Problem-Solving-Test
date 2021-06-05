a36f3bdcfb9c011b7dd2081eef552c74d6be6461
DESKTOP-O5OHVA2\Surya <surya13asofficial@gmail.com>
2021-06-05 19:27:07 +0700

    Problem 2

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [Header("Ball Movement")]
+    public Vector2 ballAngle = new Vector2(100, 100);
+    public float ballSpeed = 100f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(ballAngle.normalized * ballSpeed);
+    }
+
+}
